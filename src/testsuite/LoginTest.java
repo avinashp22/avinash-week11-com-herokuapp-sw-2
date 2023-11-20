952893045f8a064d8cbc551ba76c3dfe7c464717
avinashp22 <avinashp22@gmail.com>
2023-11-20 22:59:24 +0000

    avinash-week11-com-herokuapp-sw-2

--- src/testsuite/LoginTest.java
+++ src/testsuite/LoginTest.java
@@ -14,13 +14,33 @@ public class LoginTest extends BaseTest {
     public void setUp(){openBrowser(baseURL);}
 
     @Test
-    public void userSholdLoginSuccessfullyWithValidCredentials(){
+    public void userShouldLoginSuccessfullyWithValidCredentials(){
         driver.findElement(By.id("username")).sendKeys("tomsmith");
         driver.findElement(By.id("password")).sendKeys("SuperSecretPassword!");
         driver.findElement(By.xpath("//i[contains(text(),'Login')]")).click();
         String expected = "Secure Area";
         String actual = driver.findElement(By.xpath("//div[@class = 'example']//h2[text() = ' Secure Area']")).getText();
-        Assert.assertEquals(expected,actual);
+        Assert.assertEquals("Correct Message not Displayed",expected,actual);
+    }
+
+    @Test
+    public void verifyTheUsernameErrorMessage(){
+        driver.findElement(By.id("username")).sendKeys("xxxx");
+        driver.findElement(By.id("password")).sendKeys("SuperSecretPassword!");
+        driver.findElement(By.xpath("//i[contains(text(),'Login')]")).click();
+        String expected = "Your username is invalid!\n" + "×";
+        String actual = driver.findElement(By.xpath("//div[@class = 'flash error']")).getText();
+        Assert.assertEquals("Correct Message not Displayed",expected,actual);
+    }
+
+    @Test
+    public void verifyThePasswordErrorMessage(){
+        driver.findElement(By.id("username")).sendKeys("tomsmith");
+        driver.findElement(By.id("password")).sendKeys("xxxx");
+        driver.findElement(By.xpath("//i[contains(text(),'Login')]")).click();
+        String expected = "Your password is invalid!\n" + "×";
+        String actual = driver.findElement(By.xpath("//div[@class = 'flash error']")).getText();
+        Assert.assertEquals("Correct Message not Displayed",expected,actual);
     }
 
     @After
