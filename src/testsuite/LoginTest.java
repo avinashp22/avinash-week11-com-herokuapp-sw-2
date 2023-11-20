91892aafee6730b2591e0ce3cf82363663ad29f3
avinashp22 <avinashp22@gmail.com>
2023-11-20 13:34:44 +0000

    avinash-week11-com-herokuapp-sw-2

--- src/testsuite/LoginTest.java
+++ src/testsuite/LoginTest.java
@@ -2,31 +2,27 @@ package testsuite;
 
 import browserfactory.BaseTest;
 import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+import org.openqa.selenium.By;
 
 public class LoginTest extends BaseTest {
     String baseURL = "http://the-internet.herokuapp.com/login"; // set base url
 
     @Before
-    public void setUP(){openBrowser(baseURL);}
+    public void setUp(){openBrowser(baseURL);}
 
     @Test
-    public void nameHere1(){
-
-
-    }
-
-
-
-    @Test
-    public void nameHere2(){
-
-
-
+    public void userSholdLoginSuccessfullyWithValidCredentials(){
+        driver.findElement(By.id("username")).sendKeys("tomsmith");
+        driver.findElement(By.id("password")).sendKeys("SuperSecretPassword!");
+        driver.findElement(By.xpath("//i[contains(text(),'Login')]")).click();
+        String expected = "Secure Area";
+        String actual = driver.findElement(By.xpath("//div[@class = 'example']//h2[text() = ' Secure Area']")).getText();
+        Assert.assertEquals(expected,actual);
     }
 
     @After
     public void tearDown(){closeBrowser();}
-
 }
